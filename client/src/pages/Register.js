8f0c6ec74f1da0631f25b0fc76017c746b44e849
Manny <mhfernan35277@gmail.com>
2025-11-16 03:58:10 -0700

    registration page

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -3,13 +3,16 @@ import { useFormik } from "formik"
 import { basicSchema } from "../schemas";
 import '../styles/Register.css'
 
+function onSubmit(values, actions) {
+    console.log("submitted")
+    console.log(values)
+    console.log(actions)
+    actions.resetForm()
+
+}
 function Register() {
 
-    function onSubmit(){
-        console.log("Submitted")
-    }
-
-    const {values, errors, handleChange} = useFormik({
+    const {values, errors, touched, isSubmitting, handleChange, handleSubmit} = useFormik({
     initialValues: {
         first_name : "",
         last_name : "",
@@ -26,53 +29,95 @@ function Register() {
     return(
         <div className="registration-page">
         <div className="wrapper">
-            <div className="app-icon"> 
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-czRW6VaPHICiCYdzPCYl5PLurkqjecWy7A&s" alt="logo"/>
-            </div>
             <h1> Moto-Leaze</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
             <div className="reg-input">
                 <label htmlFor="first_name">First Name</label>
                 <input id="first_name" type="text" value= {values.first_name} 
-                onChange={handleChange} placeholder="Enter first name..." />
+                onChange={handleChange} placeholder="Enter first name..." 
+                className={errors.first_name && touched.first_name? "input-error" : ""}
+                autoComplete="off"
+                />
+                {errors.first_name && touched.first_name && 
+                <p className="error-message">{errors.first_name}</p>}
+               
             </div>
             <div className="reg-input">
                 <label htmlFor="last_name">Last Name</label>
                 <input id="last_name" type="text" value={values.last_name} placeholder="Enter last name..."
-                onChange={handleChange} />
+                onChange={handleChange} 
+                className={errors.last_name && touched.last_name? "input-error" : ""}
+                autoComplete="off"
+                />
+                {errors.last_name && touched.last_name && 
+                <p className="error-message">{errors.last_name}</p>}
             </div>
             <div className="reg-input">
                 <label htmlFor="dob">Date of Birth</label>
                 <input id="dob" type="date" value={values.dob}
-                onChange={handleChange} />
+                onChange={handleChange} 
+                className={errors.dob && touched.dob? "input-error" : ""}
+                />
+                {errors.dob && touched.dob && 
+                <p className="error-message">{errors.dob}</p>}
             </div>
             <div className="reg-input">
                 <label htmlFor="address">Home Address</label>
                 <input id="address" type="text" value={values.address}
-                onChange={handleChange} placeholder="Enter Home Address"/>
+                onChange={handleChange} placeholder="Enter Home Address"
+                className={errors.address && touched.address? "input-error" : ""}
+                autoComplete="off"
+                />
+                {errors.address && touched.address && 
+                <p className="error-message">{errors.address}</p>}
+
             </div>
             <div className="reg-input">
                 <label htmlFor="city">City</label>
                 <input id="city" type="text" value={values.city}
-                onChange={handleChange} placeholder="Enter City"/>
+                onChange={handleChange} placeholder="Enter City"
+                className={errors.city && touched.city? "input-error" : ""}
+                autoComplete="off"
+                />
+                {errors.city && touched.city && 
+                <p className="error-message">{errors.city}</p>}
+
             </div>
             <div className="reg-input">
                 <label htmlFor="state">State</label>
                 <input id="state" type="text" value={values.state}
-                onChange={handleChange} placeholder="Enter State"/>
+                onChange={handleChange} placeholder="Enter State"
+                className={errors.state && touched.state? "input-error" : ""}
+                autoComplete="off"
+                />
+                {errors.state && touched.state && 
+                <p className="error-message">{errors.state}</p>}
+
             </div>
             <div className="reg-input">
                 <label htmlFor="zipcode">Zipcode</label>
                 <input id="zipcode" type="text" value={values.zipcode}
-                onChange={handleChange} placeholder="Enter Zipcode"/>
+                onChange={handleChange} placeholder="Enter Zipcode"
+                className={errors.zipcode? "input-error" : ""}
+                autocomplete="off"
+                />
+                {errors.zipcode && touched.zipcode && 
+                <p className="error-message">{errors.zipcode}</p>}
+
             </div>
             <div className="reg-input">
                 <label htmlFor="password">Password</label>
                 <input id="password" type="password" value={values.password}
-                onChange={handleChange} placeholder="Enter Password"/>
+                onChange={handleChange} placeholder="Enter Password"
+                className={errors.password && touched.password? "input-error" : ""}
+                autoComplete="off"
+                />
+                {errors.password && touched.password && 
+                <p className="error-message">{errors.password}</p>}
+
             </div>
 
-            <button type="submit">Create User</button>
+            <button disabled={isSubmitting} type="submit">Create User</button>
         </form >
         </div>
         </div>
--- client/src/schemas/index.js
+++ client/src/schemas/index.js
@@ -3,13 +3,16 @@ import * as yup from "yup";
 const passwordRules = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{5,}$/;
 
 export const basicSchema = yup.object().shape({
-    first_name : yup.string().required("Required"),
-    last_name : yup.string().last_name.required("Required"),
-    // password : yup.string.min(5).matches(passwordRules, {message: "Please create a stronger password!"}).required("Required"),
-    // // confirmPassword : yup.string().oneOf([yup.ref("password"), null], "Passwords must match").required("Required"),
-    // dob : yup.date().dob.required("Required"),
-    // address : yup.string().address.required("Required"),
-    // city : yup.string().city.required("Required"),
-    // state : yup.string().state.required("Required"),
-    // zipcode : yup.number().positive().integer().required("Must enter zipcode").max(5),
+    first_name : yup.string().required("First name is required"),
+    last_name : yup.string().required("Last name is required"),
+    password : yup
+    .string()
+    .min(5).matches(passwordRules, {message: "Please create a stronger password!"})
+    .required("Password is required"),
+    // confirmPassword : yup.string().oneOf([yup.ref("password"), null], "Passwords must match").required("Required"),
+    dob : yup.date().required("Date of birth is required"),
+    address : yup.string().required("Address is required"),
+    city : yup.string().required("City name is required"),
+    state : yup.string().required("State is required"),
+    zipcode : yup.number().positive().integer().required("Must be a valid zipcode")
 });
